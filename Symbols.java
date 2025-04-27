57eac5da8f68749b71f92c593bd034e7dbcf4780
Selen Altınsoy <152925698+SSSelen@users.noreply.github.com>
2025-04-28 01:07:00 +0300

    Create Symbols.java

---
+++ Symbols.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class ExistingSymbolException extends Exception{
+  public ExistingSymbolException(String message){
+        super(message);
+        }
+  }
+
+class Symbols extends Elements{
+  private Set<String> symbols;
+
+  public Symbols(){
+    this.symbols=new Hashset<>();
+    }
+
+  public Set<String> getSymbols(){
+    return this.symbols;
+    }
+  public void setSymbols(Set<String> symbols){
+    this.symbols=symbols;
+  }
+
+  private void addSymbol(String name) throws ExistingSymbolException{
+    if(isValid(name)==false){
+      System.out.println("Warning: Symbol is not alphanumeric, it will be ignored!");
+      return;
+    }
+    if (symbols.contains(name) {
+            throw new ExistingSymbolException("Warning: Symbol '" + name + "' exists!");
+        } else {
+            symbols.add(name);
+        }
+    }
+
+  private void printSymbols() {
+        if (symbols.isEmpty()) {
+            System.out.println("No symbols declared!");
+            return;
+        }
+        System.out.println("Declared symbols:");
+        for (String s: symbols) {
+            System.out.println(s);
+        }
+    }
+
+}
