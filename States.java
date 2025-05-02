df8cd2bc3900d909263e7d89ceac39b808bad96b
burcudumanl <153668213+burcudumanl@users.noreply.github.com>
2025-05-02 15:00:46 +0300

    Update States.java

--- States.java
+++ States.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 
-class States extends Elements implements CLEAR {
+class States extends Elements implements clear{
     private Set<String> states = new HashSet<>();
     private Set<String> finalStates = new HashSet<>();
     private String initialState;
