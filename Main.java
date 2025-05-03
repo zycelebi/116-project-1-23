67bc9e82c9884414c8d63f6b0a8021e6803077ed
Selen Altınsoy <152925698+SSSelen@users.noreply.github.com>
2025-05-03 22:44:27 +0300

    Update and rename FSMDesigner.java to Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FSMDesigner {
+public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
